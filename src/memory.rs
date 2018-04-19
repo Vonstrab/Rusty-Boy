fbcd1220836ca2477965d55ec64e1ad088402105
ivan.delgado@etu.upmc.fr <delgado.ivan@protonmail.com>
2018-04-19 20:50:44 +0200

    improve memory

--- src/memory.rs
+++ src/memory.rs
@@ -1,5 +1,11 @@
 pub struct Memory {
-    ram: [u8; 0xFFFF],
+    empty_zone : [u8;0x80-0x4c] //adress FE80 - FEA0 
+    sprite_attribute_memory : [u8;160] //adress FEA0 -FE00
+    internal_ram: [u8; 8 * 1024],     //adress E000 - A000
+    switchable_ram: [u8; 8 * 1024],   //adress C000 - A000
+    video_ram: [u8; 8 * 1024],        //adress A000 - 8000
+    rom_bank_switch: [u8; 16 * 1024], // adress 8000 - 4000
+    rom_bank0: [u8; 16 * 1024],       // adress 4000 - 0000
 }
 
 impl Memory {
